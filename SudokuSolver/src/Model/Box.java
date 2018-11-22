52a1ab10bdde63909bdc57ff9fccb0aafd846bde
Lukas <m3ntozz911@web.de>
2018-11-22 18:03:32 +0100

    added cell-model; updated box

--- SudokuSolver/src/Model/Box.java
+++ SudokuSolver/src/Model/Box.java
@@ -2,4 +2,24 @@ package Model;
 
 public class Box {
 
+	private String name;
+	private Cell[] cells;
+	private String[] neighbors;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Cell[] getCells() {
+		return cells;
+	}
+
+	public void setCells(Cell[] cells) {
+		this.cells = cells;
+	}
+
 }
---
+++ SudokuSolver/src/Model/Cell.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class Cell {
+
+	private String position;
+	private int value;
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+}
